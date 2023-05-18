b17ba8069fa1dd67efae90ec05196241c4b7d38e
rfathullin <rfathullin@luxms.com>
2023-05-18 19:38:06 +0300

    feat: add websocket

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -2,6 +2,8 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 
+import * as WebSocket from "ws";
+
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
     const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
@@ -15,3 +17,21 @@ export const httpServer = http.createServer(function (req, res) {
         res.end(data);
     });
 });
+
+
+// const wss = new WebSocketServer.Server( {httpServer} )
+// const wss = WebSocketServer({httpServer})
+
+const wss = new WebSocket.Server({httpServer})
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if ( !client !== ws && client.readyState === WebSocket.OPEN ) {
+                client.send(data)
+            }
+        })
+    })
+} )
+
+httpServer.listen(8787, () => console.log(`Server started on port `, 8787))
