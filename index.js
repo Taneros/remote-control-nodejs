63c636b3f53226747ba8013f231ba53f6812b8ff
rfathullin <rfathullin@luxms.com>
2023-05-19 19:31:31 +0300

    fix: websocket connection

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
+import WebSocket, { WebSocketServer } from 'ws';
 import { httpServer } from "./src/http_server/index.js";
-import { mouse } from "@nut-tree/nut-js";
+import { mouse, left, right, up, down } from "@nut-tree/nut-js";
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
-httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer( {server: httpServer} )
+
+wss.on('connection', function connection(ws) {
+  ws.on( 'message', function incoming ( data ) {
+      
+
+
+    (async () => {
+      await mouse.move(left(500));
+      await mouse.move(up(500));
+      await mouse.move(right(500));
+      await mouse.move(down(500))
+  })();
+
+        wss.clients.forEach(function each(client) {
+            if ( !client !== ws && client.readyState === WebSocket.OPEN ) {
+              if ( typeof data === 'string' ) {
+                console.log(`index.js - line: 18 ->> client.send TEXT`)
+                client.send( data )
+              } else if (data instanceof Buffer) {
+                console.log(`index.js - line: 21 ->> client.send BUFFER`, )
+                const text = data.toString( 'utf8' );
+                client.send( text )
+              }
+            }
+        })
+    })
+} )
+
+httpServer.listen( HTTP_PORT );
+
+// const wz = new WebSocket( 'ws://localhost:8181' )
+
+// wz.onopen = () => {
+//   console.log('Connection opened!');
+// }
+
+// wz.onmessage = ( {data} ) => {
+//   if (typeof data === 'string') {
+//     console.log('Received message:', data);
+//   } else if (data instanceof Buffer) {
+//     const text = data.toString('utf8');
+//     console.log('Received binary message:', text);
+//   }
+// }
+// wz.onclose = () => {wz = null}
--- src/http_server/index.js
+++ src/http_server/index.js
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as http from 'http';
 
-import * as WebSocket from "ws";
+
 
 export const httpServer = http.createServer(function (req, res) {
     const __dirname = path.resolve(path.dirname(''));
@@ -19,19 +19,3 @@ export const httpServer = http.createServer(function (req, res) {
 });
 
 
-// const wss = new WebSocketServer.Server( {httpServer} )
-// const wss = WebSocketServer({httpServer})
-
-const wss = new WebSocket.Server({httpServer})
-
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        wss.clients.forEach(function each(client) {
-            if ( !client !== ws && client.readyState === WebSocket.OPEN ) {
-                client.send(data)
-            }
-        })
-    })
-} )
-
-httpServer.listen(8787, () => console.log(`Server started on port `, 8787))
